207ed3458581bd226c5bf923319b2eb11eaa8517
jackshcn1 <rishvin787@gmail.com>
2026-09-18 13:31:53 +0530

    Delete app directory

--- app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Welcome to Atelier HR</h1>
-      <p>Use the menu above to view employees or the org chart. Log in first if you haven't.</p>
-    </div>
-  );
-}
